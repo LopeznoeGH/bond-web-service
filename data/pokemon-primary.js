7c27dc6660edb73351ebfb2f3502035ba11c8969
Noe <lopeznoe38@yahoo.com>
2021-03-05 02:48:01 -0800

    add pokedex and primary

---
+++ data/pokemon-primary.js
@@ -0,0 +1,259 @@
+{
+	"title": "Pokemon Sorted by Primary Type",
+	"Pokemon":[
+
+
+                {
+                "Name":"Caterpie",
+                "Generation":1,
+                "Primary":"Bug",
+                "Secondary":"NONE",
+                "Pokedex":10,
+                "Image":"caterpie.jpg",
+
+                },
+                {
+                "Name":"Metapod",
+                "Generation":1,
+                "Primary":"Bug",
+                "Secondary":"NONE",
+                "Pokedex":11,
+                "Image":"metapod.jpg",
+    
+                  },
+              {
+                "Name":"Butterfree",
+                "Generation":1,
+                "Primary":"Bug",
+                "Secondary":"Flying",
+                "Pokedex":12,
+                "Image":"butterfree.jpg",
+    
+              },
+                {
+                "Name":"Pikachu",
+                "Generation":1,
+                "Primary":"Electric",
+                "Secondary":"NONE",
+                "Pokedex":25,
+                "Image":"pikachu.jpg",
+
+               },
+               {
+				"Name":"Raichu",
+                "Generation":1,
+                "Primary":"Electric",
+                "Secondary":"NONE",
+                "Pokedex":26,
+                "Image":"raichu.jpg",
+
+			  },
+              {
+				"Name":"Charmander",
+                "Generation":1,
+                "Primary":"Fire",
+                "Secondary":"NONE",
+                "Pokedex":4,
+                "Image":"charmander.jpg",
+
+			  },
+              {
+				"Name":"Charmeleon",
+                "Generation":1,
+                "Primary":"Fire",
+                "Secondary":"NONE",
+                "Pokedex":5,
+                "Image":"charmeleon.jpg",
+
+			  },
+
+              {
+				"Name":"Charizard",
+                "Generation":1,
+                "Primary":"Fire",
+                "Secondary":"Flying",
+                "Pokedex":6,
+                "Image":"charizard.jpg",
+
+			  },
+                
+			  {
+				"Name":"Bulbasaur",
+                "Generation":1,
+                "Primary":"Grass",
+                "Secondary":"Poison",
+                "Pokedex":1,
+                "Image":"bulbasaur.jpg",
+
+			  },
+              {
+				"Name":"Ivysaur",
+                "Generation":1,
+                "Primary":"Grass",
+                "Secondary":"Poison",
+                "Pokedex":2,
+                "Image":"ivysaur.jpg",
+
+			  },
+              {
+				"Name":"Venusaur",
+                "Generation":1,
+                "Primary":"Grass",
+                "Secondary":"Poison",
+                "Pokedex":3,
+                "Image":"vensuar.jpg",
+
+			  },
+              {
+				"Name":"Weedle",
+                "Generation":1,
+                "Primary":"Grass",
+                "Secondary":"Poison",
+                "Pokedex":13,
+                "Image":"weedle.jpg",
+
+			  },
+              {
+				"Name":"Kakuna",
+                "Generation":1,
+                "Primary":"Grass",
+                "Secondary":"Poison",
+                "Pokedex":14,
+                "Image":"kakuna.jpg",
+
+			  },
+              {
+				"Name":"Beedrill",
+                "Generation":1,
+                "Primary":"Grass",
+                "Secondary":"Poison",
+                "Pokedex":15,
+                "Image":"beedrill.jpg",
+
+			  },
+              {
+				"Name":"Sandshrew",
+                "Generation":1,
+                "Primary":"Ground",
+                "Secondary":"NONE",
+                "Pokedex":27,
+                "Image":"sandshrew.jpg",
+
+			  },
+,
+              {
+				"Name":"Squirtle",
+                "Generation":1,
+                "Primary":"Water",
+                "Secondary":"NONE",
+                "Pokedex":7,
+                "Image":"squirtle.jpg",
+
+			  },
+              {
+				"Name":"Wartortle",
+                "Generation":1,
+                "Primary":"Water",
+                "Secondary":"NONE",
+                "Pokedex":8,
+                "Image":"wartortle.jpg",
+
+			  },
+              {
+				"Name":"Blastoise",
+                "Generation":1,
+                "Primary":"Water",
+                "Secondary":"NONE",
+                "Pokedex":9,
+                "Image":"blastoise.jpg",
+
+			  },
+
+
+              {
+				"Name":"Pidgey",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"Flying",
+                "Pokedex":16,
+                "Image":"pidgey.jpg",
+
+			  },
+              {
+				"Name":"Pidgeotto",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"Flying",
+                "Pokedex":17,
+                "Image":"pidgeotto.jpg",
+
+			  },
+              {
+				"Name":"Pidgeot",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"Flying",
+                "Pokedex":18,
+                "Image":"pidgeot.jpg",
+
+			  },
+              {
+				"Name":"Rattata",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"NONE",
+                "Pokedex":19,
+                "Image":"rattata.jpg",
+
+			  },
+              {
+				"Name":"Raticate",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"NONE",
+                "Pokedex":20,
+                "Image":"raticate.jpg",
+
+			  },
+              {
+				"Name":"Spearow",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"Flying",
+                "Pokedex":21,
+                "Image":"spearow.jpg",
+
+			  },
+              {
+				"Name":"Fearow",
+                "Generation":1,
+                "Primary":"Normal",
+                "Secondary":"Flying",
+                "Pokedex":22,
+                "Image":"fearow.jpg",
+
+			  },
+              {
+				"Name":"Ekans",
+                "Generation":1,
+                "Primary":"Poison",
+                "Secondary":"NONE",
+                "Pokedex":23,
+                "Image":"ekans.jpg",
+
+			  },
+              {
+				"Name":"Arbok",
+                "Generation":1,
+                "Primary":"Poison",
+                "Secondary":"NONE",
+                "Pokedex":24,
+                "Image":"arbok.jpg",
+
+			  },
+              
+
+
+                           
+            ]
+}
